b06dbede231490feb86daf721435816ad672f506
yuhaowin <yuhao.coding@gmail.com>
2020-03-15 16:20:17 +0800

    remove webservice

--- src/main/java/com/yuhaowin/webservice/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.webservice.yuhaowin.com/")
-package com.yuhaowin.webservice.client;
--- src/main/java/com/yuhaowin/webservice/messagetemplate/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://facade.utc.js189.cn/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.yuhaowin.webservice.messagetemplate;
